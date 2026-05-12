2fcdf01fc2caea43c3e2f69c4d7036419a8186df
unknown <elliotttmiller@hotmail.com>
2026-05-12 09:53:49 -0500

    Update - Checkpoint

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/gap-tool/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
